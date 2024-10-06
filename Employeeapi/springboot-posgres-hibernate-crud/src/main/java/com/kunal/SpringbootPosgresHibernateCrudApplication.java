12bb789c358e2fc832534037c8ca3f8c2611feaa
Kunal Nathani <48233889+kunal-nathani@users.noreply.github.com>
2024-10-06 11:31:19 +0530

    Add files via upload

---
+++ Employeeapi/springboot-posgres-hibernate-crud/src/main/java/com/kunal/SpringbootPosgresHibernateCrudApplication.java
@@ -0,0 +1,13 @@
+package com.kunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPosgresHibernateCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPosgresHibernateCrudApplication.class, args);
+	}
+
+}
---
+++ Employeeapi/springboot-posgres-hibernate-crud/src/test/java/com/kunal/SpringbootPosgresHibernateCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPosgresHibernateCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
